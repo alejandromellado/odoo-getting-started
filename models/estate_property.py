2da55b381fbeaef07c300003ead09a6c500c972d
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-09 19:02:21 -0700

    Initial commit

---
+++ models/estate_property.py
@@ -0,0 +1,49 @@
+from datetime import date
+
+from dateutil.relativedelta import relativedelta
+
+from odoo import models, fields
+
+
+class EstateProperty(models.Model):
+    _name = 'estate.property'
+    _description = 'An estate property listing.'
+
+    name = fields.Char(required=True, string='Title')
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date(
+        string='Available From',
+        copy=False,
+        default=lambda self: date.today() + relativedelta(months=+3)
+    )
+    expected_price = fields.Float(required=True)
+    selling_price = fields.Float(readonly=True, copy=False)
+    bedrooms = fields.Integer(default=2)
+    living_area = fields.Integer(string='Living Area (sqm)')
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer(string='Garden Area (sqm)')
+    garden_orientation = fields.Selection(
+        string='Garden Orientation',
+        selection=[
+            ('north', 'North'),
+            ('south', 'South'),
+            ('east', 'East'),
+            ('west', 'West'),
+        ],
+    )
+    active = fields.Boolean(default=True)
+    state = fields.Selection(
+        required=True,
+        copy=False,
+        selection=[
+            ('new', 'New'),
+            ('offer_received', 'Offer Received'),
+            ('offer_accepted', 'Offer Accepted'),
+            ('sold', 'Sold'),
+            ('canceled', 'Canceled'),
+        ],
+        default='new',
+    )
