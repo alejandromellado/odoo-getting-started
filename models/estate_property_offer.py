8a888ec3ff824af7994c53232a914cf1ef54e976
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-13 19:27:46 -0700

    added create_date, validity, date_deadline fields

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -1,4 +1,6 @@
-from odoo import models, fields
+from datetime import date
+from dateutil.relativedelta import relativedelta
+from odoo import models, fields, api
 
 
 class EstatePropertyOffer(models.Model):
@@ -15,3 +17,25 @@ class EstatePropertyOffer(models.Model):
     )
     partner_id = fields.Many2one('res.partner', required=True, string='Partner')
     property_id = fields.Many2one('estate.property', required=True, string='Property')
+
+    # Deadline
+    create_date = fields.Datetime()
+    validity = fields.Integer(string='Validity (days)', default=7)
+
+    @api.depends('validity')
+    def _compute_deadline(self):
+        for record in self:
+            start = fields.Date.today() if not record.create_date else record.create_date
+            record.date_deadline = start + relativedelta(days=+record.validity)
+
+    def _inverse_deadline(self):
+        for record in self:
+            start = fields.Date.to_date(record.create_date)
+            diff = relativedelta(record.date_deadline, start)
+            record.validity = diff.days
+
+    date_deadline = fields.Date(
+        string='Deadline',
+        compute='_compute_deadline',
+        inverse='_inverse_deadline'
+    )
