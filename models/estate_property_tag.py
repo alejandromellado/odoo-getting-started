05afbdade4f366154488d136771424f838de8a87
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-16 15:40:29 -0700

    [IMP] estate: order records by name
    Ordering by name is convenient for these particular models.

--- models/estate_property_tag.py
+++ models/estate_property_tag.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class EstatePropertyTag(models.Model):
     _name = 'estate.property.tag'
     _description = 'Multiple tags can be associated to multiple properties to aid description and search.'
+    _order = "name"
 
     name = fields.Char(required=True)
 
--- models/estate_property_type.py
+++ models/estate_property_type.py
@@ -4,6 +4,7 @@ from odoo import models, fields
 class EstatePropertyType(models.Model):
     _name = 'estate.property.type'
     _description = 'Represents the type of property that is listed.'
+    _order = "name"
 
     name = fields.Char(required=True)
     property_ids = fields.One2many('estate.property', 'property_type_id')
