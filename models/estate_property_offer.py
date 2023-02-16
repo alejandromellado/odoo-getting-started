03fb6beca10dcc9759b0ae5321af045d0e440dc4
Alejandro Mellado <alejandromm@vauxoo.com>
2023-02-15 18:31:23 -0700

    change property selling_price before marking offer as accepted

--- models/estate_property_offer.py
+++ models/estate_property_offer.py
@@ -24,11 +24,13 @@ class EstatePropertyOffer(models.Model):
             property_record = record.property_id
             if property_record.state == 'offer_accepted':
                 raise UserError('An offer has already been accepted.')
-            record.status = 'accepted'
+
             # Update related property listing
-            property_record.state = 'offer_accepted'
             property_record.selling_price = record.price
             property_record.buyer_id = record.partner_id
+            property_record.state = 'offer_accepted'
+
+            record.status = 'accepted'
         return True
 
     def action_refuse_offer(self):
