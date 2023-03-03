dcf763aa0db29137008d6136ba1809a05c956090
Alejandro Mellado <alejandromm@vauxoo.com>
2023-03-02 21:41:32 -0700

    [IMP] estate: create user groups with access rights
    Two access groups represent different type of users of the app, with managers being able to see everything and create management related records, and agents focusing on properties.

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -7,6 +7,7 @@
         'base',
     ],
     'data': [
+        'security/security.xml',
         'security/ir.model.access.csv',
         'views/estate_property_views.xml',
         'views/estate_property_tag_views.xml',
